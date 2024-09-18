d7d496921108a09630a515b970f88e722900a6a5
Gayan B <gayan@ymail.com>
2024-09-18 16:30:33 +1000

    Updated unit tests

--- src/test/java/com/dxbair/services/flightbooking/test/ApplicationTests.java
+++ src/test/java/com/dxbair/services/flightbooking/test/ApplicationTests.java
@@ -1,11 +1,11 @@
 package com.dxbair.services.flightbooking.test;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class ApplicationTests {
 
